852929b9a07b52705a592036899786845e2d6f4b
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-23 19:20:53 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist 
+  attr_accessor :name 
+  @@all=[]
+
+def initialize(name)
+  @@all = name 
+  @@all << self 
+end
+
+def self.all
+  @@all 
+end 
+
+def songs 
+  song.all.select { |song| song.artist =self} 
+end 
+
+def add_song(song)
+  song.artist = self 
+end 
+
+def self.find_or_create_by_name(name)
+  self.find(name) ? self.find(name) : self.new(name)
+end 
+
+def print_songs
+  songs.each { |song| puts song.artist.name}
+end 
+end
